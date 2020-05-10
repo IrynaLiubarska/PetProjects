896bbba5e84d76bf1407c27b016ce615c4bfd3d0
IrynaLiubarska <i.v.tsukanova@gmail.com>
2020-05-10 18:57:37 +0200

    inline code

--- berliner-clock/src/main/java/clock/BerlinerClock.java
+++ berliner-clock/src/main/java/clock/BerlinerClock.java
@@ -63,9 +63,7 @@ public class BerlinerClock {
         List<TimeBlock> representation5MinutesIntervals = createTimeBlocksFor5MinutesInterval();
         int lessThan5MinutesTime = minutes % 5;
         int intervalsWith5Minutes = minutes / 5;
-        if (lessThan5MinutesTime != 0) {
-            representEach1Minutes(lessThan5MinutesTime);
-        }
+        if (lessThan5MinutesTime != 0)             representEach1Minutes(lessThan5MinutesTime);
         if (intervalsWith5Minutes != 0) {
             for (int i = 0; i < intervalsWith5Minutes; i++) {
                 representation5MinutesIntervals.get(i).setLighted(true);
